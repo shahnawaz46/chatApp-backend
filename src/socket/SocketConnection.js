3b83472701be3f891753aacb0d3a4c72dc161d3e
shahnawaz <shahnawazmd791@gmail.com>
2022-09-02 18:11:09 +0530

    update socket event

--- src/socket/SocketConnection.js
+++ src/socket/SocketConnection.js
@@ -1,126 +1,175 @@
-const { upadateNotificationInUserCollection,
-    addFriendInUserCollection,
-    getLoginUserData,
-    userLogout,
-    addMessagesToTheDatabase,
-    getMessagesfromTheDatabase,
-    removeFriendFromUserCollection,
-    removeMessageFromTheDatabase,
-    allMessagesSeen } = require('./SocketController');
+const {
+  upadateNotificationInUserCollection,
+  addFriendInUserCollection,
+  getLoginUserData,
+  userLogout,
+  addMessagesToTheDatabase,
+  getMessagesfromTheDatabase,
+  removeFriendFromUserCollection,
+  removeMessageFromTheDatabase,
+  allMessagesSeen,
+} = require("./SocketController");
 
-
-let allOnlineUser = {}
+let allOnlineUser = {};
 
 const socketConnection = (io) => {
 
-    io.on("connection", (socket) => {
+  io.on("connection", (socket) => {
 
-        socket.on("disconnect", () => {
-            for (var key in allOnlineUser) {
-                if (allOnlineUser[key] === socket.id) {
-                    delete allOnlineUser[key]
-                    break
-                }
-            }
-        })
+    socket.on("disconnect",async () => {
+      for (var key in allOnlineUser) {
 
-        socket.on("online_user", async (userId) => {
-            allOnlineUser[userId] = socket.id
+        if (allOnlineUser[key] === socket.id) {
 
-            const onlineUser = await getLoginUserData(userId)
-            io.to(allOnlineUser[userId]).emit("user_online", onlineUser)
-
-            onlineUser?.friends.forEach(async (friend) => {
-                if (friend.online) {
-                    const onlineUser = await getLoginUserData(friend._id)
-                    io.to(allOnlineUser[friend._id]).emit("user_online", onlineUser)
-                }
-            })
-        })
-
-        socket.on("offline_user", async (userId) => {
-            const offlineUser = await userLogout(userId)
+            const offlineUser = await userLogout(key);
 
             offlineUser?.friends.forEach(async (friend) => {
                 if (friend.online) {
-                    const onlineUser = await getLoginUserData(friend._id)
-                    io.to(allOnlineUser[friend._id]).emit("user_online", onlineUser)
+                const onlineUser = await getLoginUserData(friend._id);
+                io.to(allOnlineUser[friend._id]).emit("user_online", onlineUser);
                 }
-            })
+          });
 
-        })
+          delete allOnlineUser[key];
+          break;
+        }
+      }
+    });
 
-        socket.on("friend_request_send", async (request) => {
-            const updatedUser = await upadateNotificationInUserCollection(request, '$push')
+    socket.on("online_user", async (userId) => {
+      allOnlineUser[userId] = socket.id;
 
-            io.to(allOnlineUser[request.receiverId]).emit('friend_request_receive', updatedUser)
-        })
+      const onlineUser = await getLoginUserData(userId);
+      io.to(allOnlineUser[userId]).emit("user_online", onlineUser);
 
-        socket.on("accept_friend_request", async (bothIds) => {
-            const updatedUser = await addFriendInUserCollection(bothIds)
+      onlineUser?.friends.forEach(async (friend) => {
+        if (friend.online) {
+          const onlineUser = await getLoginUserData(friend._id);
+          io.to(allOnlineUser[friend._id]).emit("user_online", onlineUser);
+        }
+      });
+    });
 
-            // emit event and add friend in the friend list of sender person (who send the friend request)
-            io.to(allOnlineUser[bothIds.senderId]).emit("request_accepted", updatedUser.sender)
+    socket.on("offline_user", async (userId) => {
+      const offlineUser = await userLogout(userId);
 
-            // emit event and add friend in the friend list of receiver person (who accept the friend request)
-            io.to(allOnlineUser[bothIds.receiverId]).emit("you_accept_request", updatedUser.receiver)
-        })
+      offlineUser?.friends.forEach(async (friend) => {
+        if (friend.online) {
+          const onlineUser = await getLoginUserData(friend._id);
+          io.to(allOnlineUser[friend._id]).emit("user_online", onlineUser);
+        }
+      });
+    });
 
-        socket.on("reject_friend_request", async (request) => {
-            const updatedUser = await upadateNotificationInUserCollection(request, '$pull')
+    socket.on("friend_request_send", async (request) => {
+      const updatedUser = await upadateNotificationInUserCollection(
+        request,
+        "$push"
+      );
 
-            io.to(allOnlineUser[request.receiverId]).emit('friend_request_receive', updatedUser)
-        })
+      io.to(allOnlineUser[request.receiverId]).emit(
+        "friend_request_receive",
+        updatedUser
+      );
+    });
 
+    socket.on("accept_friend_request", async (bothIds) => {
+      const updatedUser = await addFriendInUserCollection(bothIds);
 
-        socket.on("remove_friend", async (bothIds) => {
-            const updatedUser = await removeFriendFromUserCollection(bothIds)
+      // emit event and add friend in the friend list of sender person (who send the friend request)
+      io.to(allOnlineUser[bothIds.senderId]).emit(
+        "request_accepted",
+        updatedUser.sender
+      );
 
-            const key = [bothIds.loginId, bothIds.removerId].sort().join('-')
+      // emit event and add friend in the friend list of receiver person (who accept the friend request)
+      io.to(allOnlineUser[bothIds.receiverId]).emit(
+        "you_accept_request",
+        updatedUser.receiver
+      );
+    });
 
-            io.to(allOnlineUser[bothIds.loginId]).emit("friend_remove", { updatedUser: updatedUser.sender, key })
+    socket.on("reject_friend_request", async (request) => {
+      const updatedUser = await upadateNotificationInUserCollection(
+        request,
+        "$pull"
+      );
 
-            io.to(allOnlineUser[bothIds.removerId]).emit("friend_remove", { updatedUser: updatedUser.remover, key })
+      io.to(allOnlineUser[request.receiverId]).emit(
+        "friend_request_receive",
+        updatedUser
+      );
+    });
 
-            await removeMessageFromTheDatabase(key)
-        })
+    socket.on("remove_friend", async (bothIds) => {
+      const updatedUser = await removeFriendFromUserCollection(bothIds);
 
+      const key = [bothIds.loginId, bothIds.removerId].sort().join("-");
 
-        // messages start from here
-        socket.on("send_message", async (messageDetail) => {
-            const key = [messageDetail.senderId, messageDetail.receiverId].sort().join('-')
+      io.to(allOnlineUser[bothIds.loginId]).emit("friend_remove", {
+        updatedUser: updatedUser.sender,
+        key,
+      });
 
-            if (allOnlineUser[messageDetail.receiverId])
-                io.to(allOnlineUser[messageDetail.receiverId]).emit('receive_message', messageDetail)
+      io.to(allOnlineUser[bothIds.removerId]).emit("friend_remove", {
+        updatedUser: updatedUser.remover,
+        key,
+      });
 
-            else {
-                io.to(allOnlineUser[messageDetail.senderId]).emit("when_receiver_offline", { key, messageDetail })
-                await addMessagesToTheDatabase(key, messageDetail)
-            }
-        })
+      await removeMessageFromTheDatabase(key);
+    });
 
-        socket.on("store_message", async (messageDetail) => {
-            const key = [messageDetail.senderId, messageDetail.receiverId].sort().join('-')
+    // messages start from here
+    socket.on("send_message", async (messageDetail) => {
+      const key = [messageDetail.senderId, messageDetail.receiverId]
+        .sort()
+        .join("-");
 
-            io.to(allOnlineUser[messageDetail.senderId]).to(allOnlineUser[messageDetail.receiverId]).emit('store_message_in_client', { key, messageDetail })
+      if (allOnlineUser[messageDetail.receiverId])
+        io.to(allOnlineUser[messageDetail.receiverId]).emit(
+          "receive_message",
+          messageDetail
+        );
+      else {
+        io.to(allOnlineUser[messageDetail.senderId]).emit(
+          "when_receiver_offline",
+          { key, messageDetail }
+        );
+        await addMessagesToTheDatabase(key, messageDetail);
+      }
+    });
 
-            await addMessagesToTheDatabase(key, messageDetail)
-        })
+    socket.on("store_message", async (messageDetail) => {
+      const key = [messageDetail.senderId, messageDetail.receiverId]
+        .sort()
+        .join("-");
 
-        socket.on("retrieve_message", async (_id) => {
-            const messagesObj = await getMessagesfromTheDatabase(_id)
+      io.to(allOnlineUser[messageDetail.senderId])
+        .to(allOnlineUser[messageDetail.receiverId])
+        .emit("store_message_in_client", { key, messageDetail });
 
-            Object.keys(messagesObj).length > 0 && io.to(allOnlineUser[_id]).emit('retrieve_message_client', { messagesObj })
-        })
+      await addMessagesToTheDatabase(key, messageDetail);
+    });
 
-        socket.on("messages_seen", async ({ key, messages }) => {
-            const [id_1, id_2] = key.split('-')
+    socket.on("retrieve_message", async (_id) => {
+      const messagesObj = await getMessagesfromTheDatabase(_id);
 
-            io.to(allOnlineUser[id_1]).to(allOnlineUser[id_2]).emit("message_seen_client", { key, messages })
+      Object.keys(messagesObj).length > 0 &&
+        io
+          .to(allOnlineUser[_id])
+          .emit("retrieve_message_client", { messagesObj });
+    });
 
-            await allMessagesSeen(key)
-        })
-    })
-}
+    socket.on("messages_seen", async ({ key, messages }) => {
+      const [id_1, id_2] = key.split("-");
+
+      io.to(allOnlineUser[id_1])
+        .to(allOnlineUser[id_2])
+        .emit("message_seen_client", { key, messages });
+
+      await allMessagesSeen(key);
+    });
+  });
+};
 
 module.exports = socketConnection;
